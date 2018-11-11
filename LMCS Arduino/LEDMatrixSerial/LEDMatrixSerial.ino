920f1aff5adc77a092071a4f7b70214ef091348b
TylerTimoJ <44880900+TylerTimoJ@users.noreply.github.com>
2018-11-11 00:21:31 -0700

    Updated .ino for new drawing mode support

---
+++ LMCS Arduino/LEDMatrixSerial/LEDMatrixSerial.ino
@@ -0,0 +1,121 @@
+#include "FastLED.h"
+
+// How many leds in your strip?
+#define NUM_LEDS 256
+
+int width = 16;
+int height = 16;
+
+int drawIndex = 0;
+int x;
+int y;
+byte pixelType = 0;
+char drawIn[4];
+char frameIn[NUM_LEDS*3];
+
+
+// For led chips like Neopixels, which have a data line, ground, and power, you just
+// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
+// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
+#define DATA_PIN 3
+//#define CLOCK_PIN 13
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  // Uncomment/edit one of the following lines for your leds arrangement.
+  // FastLED.addLeds<TM1803, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<TM1804, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<TM1809, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+  // FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  // FastLED.addLeds<APA104, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<UCS1903, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<UCS1903B, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<GW6205, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<GW6205_400, DATA_PIN, RGB>(leds, NUM_LEDS);
+
+  // FastLED.addLeds<WS2801, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<SM16716, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<LPD8806, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<P9813, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<APA102, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<DOTSTAR, RGB>(leds, NUM_LEDS);
+
+  // FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<SM16716, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<LPD8806, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<P9813, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<DOTSTAR, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+
+      for (int i = 0; i < NUM_LEDS; i++)
+      {
+        leds[i] = CRGB::Black;
+      }
+
+      FastLED.show();
+  
+  Serial.begin(256000);
+}
+
+void loop() {
+
+}
+
+void serialEvent() {
+  pixelType = Serial.read();
+
+  switch (pixelType) {
+    case 0:
+    //draw mode
+    
+      Serial.readBytes(drawIn, 5);
+      x = (int)drawIn[0];
+      y = (int)drawIn[1];
+      drawIndex = (int)(x*width)+y;
+      leds[drawIndex] = CRGB((int)drawIn[2], (int)drawIn[3], (int)drawIn[4]);
+      FastLED.show();
+      Serial.flush();
+
+      break;
+
+    
+    case 1:
+    
+      //clear mode
+      for (int i = 0; i < NUM_LEDS; i++)
+      {
+        leds[i] = CRGB::Black;
+      }
+
+      FastLED.show();
+      Serial.flush();
+      break;
+    
+    case 2:
+    
+    //frame in mode
+      Serial.readBytes(frameIn, (NUM_LEDS * 3));
+      for (int i = 0; i < NUM_LEDS; i++)
+      {
+        leds[i] = CRGB(frameIn[(i * 3)+2], frameIn[(i * 3)+1], frameIn[(i * 3)]);
+      }
+
+      FastLED.show();
+      Serial.flush();
+      break;
+    
+    case 3:
+    
+      int brightnessLED = Serial.read();
+      FastLED.setBrightness(brightnessLED);
+      Serial.flush();
+
+      break;
+    
+  }
+}
